7216ee977821e51d7c1ab2365bbdc2d94ab0c836
ramandhiman6243 <73428072+ramandhiman6243@users.noreply.github.com>
2022-08-05 13:30:02 +0530

    Update EmailAlerter.cs

--- TypewiseAlert/EmailAlerter.cs
+++ TypewiseAlert/EmailAlerter.cs
@@ -4,21 +4,53 @@ namespace TypewiseAlert
 {
     public class EmailAlerter : IAlerter
     {
+        string recepient = "a.b@c.com";
+        string headerFormat = "To: {0}\n";
+
+        class BreachEmail
+        {
+            public BreachType breachType;
+            public string message;
+
+            public BreachEmail(BreachType breachType, string message)
+            {
+                this.breachType = breachType;
+                this.message = message;
+            }
+        }
+
+        BreachEmail[] breachEmails = new BreachEmail[]
+        {
+            new BreachEmail(BreachType.TOO_LOW, "Hi, the temperature is too low\n"),
+            new BreachEmail(BreachType.TOO_HIGH, "Hi, the temperature is too high\n")
+        };
+
+        string GetEmailHeader(string recepient)
+        {
+            return string.Format(headerFormat, recepient);
+        }
+
+        bool HasBreachMessage(BreachType breachType, out string message)
+        {
+            for (int i = 0; i < breachEmails.Length; i++)
+            {
+                if (breachEmails[i].breachType == breachType)
+                {
+                    message = breachEmails[i].message;
+                    return true;
+                }
+            }
+
+            message = string.Empty;
+            return false;
+        }
+
         public void Send(BreachType breachType)
         {
-            string recepient = "a.b@c.com";
-            switch (breachType)
+            if (HasBreachMessage(breachType, out string message))
             {
-                case BreachType.TOO_LOW:
-                    Console.WriteLine("To: {}\n", recepient);
-                    Console.WriteLine("Hi, the temperature is too low\n");
-                    break;
-                case BreachType.TOO_HIGH:
-                    Console.WriteLine("To: {}\n", recepient);
-                    Console.WriteLine("Hi, the temperature is too high\n");
-                    break;
-                case BreachType.NORMAL:
-                    break;
+                Console.WriteLine(GetEmailHeader(recepient));
+                Console.WriteLine(message);
             }
         }
     }

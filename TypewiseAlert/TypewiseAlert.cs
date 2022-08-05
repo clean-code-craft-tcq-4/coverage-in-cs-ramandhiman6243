dbb0598881c8670cae71e34b66c9b81f229c6d52
ramandhiman6243 <73428072+ramandhiman6243@users.noreply.github.com>
2022-08-05 18:16:19 +0530

    Add files via upload

--- TypewiseAlert/TypewiseAlert.cs
+++ TypewiseAlert/TypewiseAlert.cs
@@ -2,6 +2,16 @@
 {
     public class TypewiseAlert
     {
+        public static void checkAndAlert(IAlerter alertTarget, BatteryCharacter batteryChar, double temperatureInC)
+        {
+            BreachType breachType = checkTemperatureBreach(batteryChar, temperatureInC);
+            alertTarget.Send(breachType);
+        }
 
+        public static BreachType checkTemperatureBreach(BatteryCharacter batteryChar, double temperatureInC)
+        {
+            BreachType breachType = Temperature.classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+            return breachType;
+        }
     }
 }
